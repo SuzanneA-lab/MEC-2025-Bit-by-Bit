b1c7f6a4cb93e033c6849f7013f4d8e4189ca2db
Suzanne Abdullah <suzanneabdullah3@gmail.com>
2025-11-09 17:34:10 -0500

    all sfx options added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const info = document.getElementById("info");
+const volpic = document.getElementById("volpic");
 const myAudio = document.getElementById("sfx");
 let soundplaying = true;
 
@@ -10,10 +11,12 @@ function changevolumesetting(){
     if (soundplaying){
         myAudio.pause()
         soundplaying = false;
+        volpic.src="real_volume_off.png"
     }
 
     else{
         myAudio.play();
         soundplaying = true;
+        volpic.src="real_volume_up.png"
     }
 }
