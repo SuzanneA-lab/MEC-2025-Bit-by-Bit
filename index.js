e677550eed5a42a785d9f9986f989901abcdbe85
Suzanne Abdullah <suzanneabdullah3@gmail.com>
2025-11-09 17:09:40 -0500

    start js file code

---
+++ index.js
@@ -0,0 +1,16 @@
+const info = document.getElementById("info");
+let soundplaying = true;
+
+function openinfo(){
+
+}
+
+function changevolumesetting(){
+    if (soundplaying){
+        soundon.pause()
+    }
+
+    else{
+        soud
+    }
+}
